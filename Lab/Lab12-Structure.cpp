6546c09d71304012c96ab2611dff5eaef6565aa1
Mekha1104 <gtrcooie@gmail.com>
2023-10-05 14:12:16 +0700

    Iab12

--- Lab/Lab12-Structure.cpp
+++ Lab/Lab12-Structure.cpp
@@ -179,66 +179,71 @@
 #include <stdio.h>
 #include <string.h>
 
-    struct Student {
+struct Student {
+    char Name[30];
+    char ID[14];
+    float Scores[5];
+} typedef S;
 
-        char Name[20] ;
-        char ID[5] ;
-        float ScoreSub1 ;
-        float ScoreSub2 ;
-        float ScoreSub3 ;
-        float ScoreSub4 ;
-        float ScoreSub5 ;
-
-    } typedef S ;
-    
-        // ฟังก์ชันเพื่อคำนวณเกรด
-        char calculateGrade(float score) {
-            if (score >= 90) return 'A';
-            else if (score >= 80) return 'B';
-            else if (score >= 70) return 'C';
-            else if (score >= 60) return 'D';
-            else return 'F';
-        }
+// Function to calculate grade
+const char* calculateGrade(float score) {
+    if (score >= 85) return "A+";
+    else if (score >= 80) return " A";
+    else if (score >= 75) return "B+";
+    else if (score >= 70) return " B";
+    else if (score >= 65) return "C+";
+    else if (score >= 60) return " C";
+    else if (score >= 55) return "D+";
+    else if (score >= 50) return " D";
+    else return " F";
+}
 
 int main() {
+    S students[3];
 
-    S student[3];
+    printf("Enter the details of 3 students:\n");
 
-    printf("Enter the details of 3 students :\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Student %d:\n", i + 1);
 
-    for (int i = 0 ; i < 3 ; i++) {
-        printf("Student %d : \n", i + 1);
-        printf("Name : ");
-        scanf("%s", student[i].Name);
-        printf("ID : ");
-        scanf("%s", student[i].ID);
-        printf("Scores in Subject 1: ");
-        scanf("%f", &student[i].ScoreSub1);
-        printf("Scores in Subject 2: ");
-        scanf("%f", &student[i].ScoreSub2);
-        printf("Scores in Subject 3: ");
-        scanf("%f", &student[i].ScoreSub3);
-        printf("Scores in Subject 4: ");
-        scanf("%f", &student[i].ScoreSub4);
-        printf("Scores in Subject 5: ");
-        scanf("%f", &student[i].ScoreSub5);
+        printf("Name: ");
+        scanf("%s %s", students[i].Name,students[i].Name);
 
+        printf("ID: ");
+        scanf("%s", students[i].ID);
+
+        for (int j = 0; j < 5; j++) {
+            printf("Scores in Subject %d: ", j + 1);
+            scanf("%f", &students[i].Scores[j]);
+        }
     }
 
-    printf("\nStudent Details: \n");
-    for(int i = 0 ; i < 3 ; i++ ){
-        
-        printf("Student %d,:\n", i + 1 );
-        printf("Name : %s\n " , student[i].Name);
-        printf("ID : %s\n " , student[i].ID);
-        printf("Scores : %.0f %.f %.0f %.0f %.0f \n" , student[i].ScoreSub1 , student[i].ScoreSub2 , student[i].ScoreSub3 , student[i].ScoreSub4 , student[i].ScoreSub5);
-        printf("Grades : %c %c %c %c %c \n" , calculateGrade(student[i] . ScoreSub1) ,calculateGrade(student[i] . ScoreSub2) ,calculateGrade(student[i] . ScoreSub3) ,calculateGrade(student[i] . ScoreSub4) ,calculateGrade(student[i] . ScoreSub5));
-            
+    printf("\nStudent Details:\n");
 
-        float average = (student[i].ScoreSub1 + student[i].ScoreSub2 + student[i].ScoreSub3 + student[i].ScoreSub4 + student[i].ScoreSub5) / 5.0 ;
-        printf("Average Scores : %.1f\n" , average );
+    for (int i = 0; i < 3; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s\n", students[i].Name);
+        printf("ID: %s\n", students[i].ID);
+        printf("Scores: ");
 
+        for (int j = 0; j < 5; j++) {
+            printf("%.0f ", students[i].Scores[j]);
+        }
+        printf("\n");
 
+        printf("Grades: ");
+        for (int j = 0; j < 5; j++) {
+            printf("%s ", calculateGrade(students[i].Scores[j]));
+        }
+        printf("\n");
+
+        float sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += students[i].Scores[j];
+        }
+        float averageScore = sum / 5.0;
+        printf("Average Scores: %.1f\n\n", averageScore);
     }
-    return 0 ;
-}//end main function
+
+    return 0;
+}
